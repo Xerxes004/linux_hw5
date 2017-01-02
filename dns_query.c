f8c335f47bdc9223b92bef6bc6954d896423cf4c
Wesley Kelly <wesleykelly@cedarville.edu>
2017-01-02 14:20:13 -0500

    Print out ip address to make sure it works

--- dns_query.c
+++ dns_query.c
@@ -1,6 +1,11 @@
-int get_cname(ip_address ip, char *c_name)
+#include "dns_query.h"
+
+int get_cname(union ip_address ip, char *c_name)
 {
-	printk(KERN_INFO "Got ip: %d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+	printk(KERN_INFO "get_cname: Translating ip: %d.%d.%d.%d", ip.a[0], ip.a[1], ip.a[2], ip.a[3]);
+	// Connect to DNS
+	// Send DNS query
+	// Read DNS response
 	
 	return 0;
 }
